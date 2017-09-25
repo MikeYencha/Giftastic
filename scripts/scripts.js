bf8eb3d67f54252f412864dc5c8c2f60ad7ecba6
MikeYencha <jmyeniii@icloud.com>
2017-09-25 12:15:12 -0400

    more javascript code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
 
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=bears&limit=1&rating=pg&api_key=dc6zaTOxFJmzC";
+  // var queryURL = "https://api.giphy.com/v1/gifs/random?q=polar_bears&&rating=pg&api_key=dc6zaTOxFJmzC";
+  // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    // animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
   var base = '//api.giphy.com/v1/gifs/';
   var endpoint = 'search?q=';
   var bears = ['polar_bears','panda_bears','black_bears','brown_bears','grizzley_bears'];
   var limit = '&limit=10';
   var rating = '&rating=pg';
   var key = '&api_key=dc6zaTOxFJmzC';
+  var bearImg = $('<img>');
 
   // $("button").on("click", function()
-  // my ajax call to giphy
-  $.ajax({
-    url: queryURL,
-    method: 'GET'
-  }).done(function(response) {
-    console.log(response);
-  });
+  // // my ajax call to giphy
+  // $.ajax({
+  //   url: queryURL,
+  //   method: 'GET'
+  // }).done(function(response) {
+  //   console.log(response);
+  // });
 
   // create static buttons
   function createButtons() {
@@ -34,9 +38,27 @@ $(document).ready(function() {
     }
   }
   createButtons();
-  
-  $('#animal-btns .bear').on("click",function(){
-      console.log(this.value);
-  });
+
+  $('#animal-btns .bear').on("click",function() {
+      var bearSearch = this.value;
+      // var x = this.value;
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        bearSearch + "&api_key=dc6zaTOxFJmzC&limit=10";
+        // my ajax call to giphy
+        $.ajax({
+          url: queryURL,
+          method: 'GET'
+        }).done(function(response) {
+          // console.log(response);
+          var results = response.data;
+          console.log(results);
+          for(var i=0;i<results.length;i++) {
+            console.log(response.data[i]);
+          }
+        });
+      });
+
+      // $('#animal-gifs').append('<br /><div class="images">');
+  // });
 
 });
